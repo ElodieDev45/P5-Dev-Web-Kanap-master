8421bcab35d99933bd9dd1bff2d88180d6251557
ElodieD45 <119341273+ElodieDev45@users.noreply.github.com>
2023-05-05 13:17:56 +0200

    ajustement comments

--- front/js/product.js
+++ front/js/product.js
@@ -1,11 +1,9 @@
 //________ récupération de l'ID du produit de la page ________
 const paramsId = new URLSearchParams(window.location.search).get('id');
 console.log('id article :',paramsId);
-const apiProduct = "http://localhost:3000/api/products/" + paramsId;
-// console.log(apiProduct);
+const apiProduct = `http://localhost:3000/api/products/${paramsId}`;
 
 //________ récupération données de l'API ________
-// async function fetchData(){
 fetch(apiProduct)
 .then(response=>response.json())
 .then(productDetails=> {  
@@ -29,9 +27,8 @@ fetch(apiProduct)
     // import du contenu : p Description
     const descriptionProduct = document.querySelector('#description');
     descriptionProduct.innerText = productDetails.description;
-    // création et imbrication de plusieurs éléments option avec boucle
+    // création liste de couleur avec boucle et import dans le DOM
     let colorsList = productDetails.colors;
-    // console.log('colorsList', colorsList);
     for(let i in colorsList){
         let productColor = colorsList[i];
         let selectColor = document.querySelector("#colors");
@@ -42,33 +39,29 @@ fetch(apiProduct)
     }
 })
 
-//________ Bouton Panier (export des données)________
+//________ Bouton "Ajouter au panier" (export des données)________
 const boutonPanier = document.querySelector("#addToCart");
-//actions de clic sur boutonPanier
+//événement de clic sur boutonPanier
 boutonPanier.addEventListener("click", function (){
     const selectedColor = document.querySelector("#colors").value;
     const selectedQuantity = document.querySelector(`#quantity`).value;
     //creation constante erreur
     let isError = false;
 
-    // SI couleur est vide on affiche un message d'alerte
+    // SI couleur est vide : message d'alerte
     if (selectedColor === '') {
         isError = true;
         return alert(`Merci de sélectionner la couleur désirée`);
-        // console.log('error color');
     }
-    // SI quantité est a 0 on affiche un message d'alerte
+    // SI quantité est a 0 : message d'alerte
     if (parseInt(selectedQuantity) < 1 || selectedQuantity === '') {
         isError = true;
         return alert(`Veuillez sélectionner votre quantité`);
-        // console.log('error qte');
     }
     if (!isError){ /*s'il n'y a pas d'erreur*/
-        // je fais le reste du traitement
-        // console.log('reste du traitement')
-        // 1- on vérifie l'existence d'un tableau de données présent dans le localstorage ici appelé "cart" sinon on crée un tableau 'cart' vide
+        // 1- Verification de la présence du tableau 'cart' dans le localstorage création du tableau 'cart' vide
         let cart = /*If*/ localStorage.getItem('cart') /*alors*/ ? JSON.parse(localStorage.getItem('cart')) /*sinon*/: []; // ternaire
-        // 2- creation de l'objet cartItem du tableau Cart d'après les données de la page à récupérer pour le panier.
+        // 2- creation objet cartItem du tableau Cart (d'après données de page récupérée pour le panier).
         const cartItem = {
             color: selectedColor,
             quantity: parseInt(selectedQuantity),
@@ -77,24 +70,24 @@ boutonPanier.addEventListener("click", function (){
         
         let isInCart = false;
         
-        // on parcours (boucle) le tableau (ou on utilise la méthode .find) pour vérifier
+        // Boucle sur le tableau (ou on utilise la méthode .find) pour vérifier
         cart.forEach((item,index) => {
-            // si le produit sélectionné EXISTE dans le tableau avec la meme id et meme couleur
+            // si produit EXISTE dans le tableau avec la meme id et meme couleur
             if (item.id === paramsId && item.color === selectedColor) {
-                // on incrémente (ajoute) simplement la quantité du produit dans le tableau
+                // Incrémentation de la quantité du produit dans le tableau
                 cart[index].quantity += parseInt(selectedQuantity);
                 isInCart = true
             }
         });
     
         // SI IL N'EXISTE PAS dans le tableau
-        // on rajoute le produit complet dans le tableau
+        // Ajout du produit complet au tableau
         if(!isInCart){
             cart.push(cartItem);
         }
     
         localStorage.setItem('cart',JSON.stringify(cart))
-        // (on redirige ou non vers la page caddie)
+        // (redirection vers la page caddie)
 
         window.alert(`Votre article à bien été ajouté à votre panier : \n couleur : ${cartItem.color} \n quantité : ${cartItem.quantity}`);
     }
