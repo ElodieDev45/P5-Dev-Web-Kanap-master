8b1a0dee2d25cbb8cf12ed629c65ce2a53cd0a1b
ElodieD45 <119341273+ElodieDev45@users.noreply.github.com>
2023-05-05 13:22:23 +0200

    transfo function

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -1,4 +1,8 @@
-//selection de l'élément html
-const orderIdElement = document.querySelector("#orderId");
-//récupération du numéro de commande dans l'url
-orderIdElement.innerText = "\n" + "\n" + new URLSearchParams(window.location.search).get('order');
+function getOrderNumber(){
+    //selection de l'élément html
+    const orderIdElement = document.querySelector("#orderId");
+    //affectattion du numéro de commande dcontenu dans l'url à orderIdElement
+    orderIdElement.innerText = "\n" + "\n" + new URLSearchParams(window.location.search).get('order');
+}
+
+getOrderNumber();
